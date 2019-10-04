10cd11dd72c533bba459bf369b11762d33198d0d
Steve Nekoliczak <steve_nekoliczak@Steves-MacBook-Pro.local>
2019-10-04 15:31:18 -0500

    Fixing setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 # Setup .vimrc
-mv ~/.vimrc ~/.vimrc.bak
-cp ~/.unix_setup/.vimrc ~/.vimrc
+cp -n ~/.unix_setup/.vimrc ~/.vimrc
 
 # Setup global .gitignore
-mv ~/.gitignore ~/.gitignore.bak
-cp ~/.vim/.gitignore ~/.gitignore
+cp -n ~/.unix_setup/.gitignore ~/.gitignore
 git config --global core.excludesfile ~/.gitignore
 
 # Setup .bashrc
-mv ~/.bashrc ~/.bashrc.bak
-cp ~/.vim/.bashrc ~/.bashrc
+cp -n ~/.unix_setup/.bashrc ~/.bashrc
 
