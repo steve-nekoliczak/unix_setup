805c7ffbfaa17751a4a65eb9d42d66c63a47dabf
Steve Nekoliczak <steve.nekoliczak@protonmail.com>
2023-04-01 15:31:28 -0500

    Get barebones xmobar setup working

--- config_files/xmonad.hs
+++ config_files/xmonad.hs
@@ -2,16 +2,17 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 
 -- The main function.
-main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+-- main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+main = xmonad =<< xmobar $ myConfig
 
 -- Command to launch the bar.
 myBar = "xmobar"
 
 -- Custom PP, configure it as you like. It determines what is being written to the bar.
-myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
+-- myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
 
 -- Key binding to toggle the gap for the bar.
-toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+-- toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
 -- Main configuration, override the defaults to your liking.
 myConfig = def
--- file_handler.sh
+++ file_handler.sh
@@ -6,6 +6,7 @@ ln -s ~/.unix_setup/config_files/.gitignore ~/.gitignore
 ln -s ~/.unix_setup/config_files/.tmux.conf ~/.tmux.conf
 mkdir ~/.xmonad
 ln -s ~/.unix_setup/config_files/xmonad.hs ~/.xmonad/xmonad.hs
+ln -s ~/.unix_setup/config_files/.xmobarrc ~/.xmobarrc
 mkdir -p ~/.config/nvim/
 ln -s ~/.unix_setup/config_files/init.lua ~/.config/nvim/init.lua
 mkdir -p ~/.config/sakura/
