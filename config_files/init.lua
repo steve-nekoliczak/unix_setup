3c88a61b29c07d31e5fe6cfbb3c008d69524320e
Steve Nekoliczak <steve.nekoliczak@protonmail.com>
2023-04-05 23:56:29 -0500

    Remove non-gruvbox colors from nvim

--- config_files/init.lua
+++ config_files/init.lua
@@ -136,8 +136,6 @@ vim.cmd([[
 vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
 vim.g.gruvbox_contrast_dark = 'hard'
-vim.cmd('hi TabLineSel ctermbg=LightBlue ctermfg=Black')
-vim.cmd('highlight Normal ctermbg=234')
 vim.g.airline_theme='gruvbox'
 -- conceal chars on cursor line in normal mode only
 vim.g.indentLine_concealcursor=''
