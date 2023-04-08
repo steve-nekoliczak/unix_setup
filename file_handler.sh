79b8b0422c510b1fc2b13df2f2cde009cc4dc9fb
Steve Nekoliczak <steve.nekoliczak@protonmail.com>
2023-04-08 18:15:05 -0500

    Add .xinitrc, remove remnants of xmonad

--- file_handler.sh
+++ file_handler.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
+ln -s ~/.unix_setup/config_files/.xinitrc ~/.xinitrc
 ln -s ~/.unix_setup/config_files/.Xresources ~/.Xresources
 ln -s ~/.unix_setup/config_files/.zshrc ~/.zshrc
 ln -s ~/.unix_setup/config_files/.gitignore ~/.gitignore
 ln -s ~/.unix_setup/config_files/.tmux.conf ~/.tmux.conf
-mkdir ~/.xmonad
-ln -s ~/.unix_setup/config_files/xmonad.hs ~/.xmonad/xmonad.hs
-ln -s ~/.unix_setup/config_files/.xmobarrc ~/.xmobarrc
 mkdir -p ~/.config/nvim/
 ln -s ~/.unix_setup/config_files/init.lua ~/.config/nvim/init.lua
 mkdir -p ~/.config/sakura/
