b2014c306f7bf68a532a1e31329e825f2316ac34
Steve Nekoliczak <steve.nekoliczak@protonmail.com>
2023-12-10 20:02:09 -0600

    Add hotkeys for coc

--- config_files/init.lua
+++ config_files/init.lua
@@ -73,9 +73,31 @@ vim.api.nvim_set_keymap("n", "<C-l>", ":TmuxNavigateRight<CR>", { noremap = true
 vim.cmd('let NERDTreeShowHidden=1')
 
 -- CoC
-vim.cmd([[
-  nmap <silent> gd <Plug>(coc-definition)
-]])
+local keyset = vim.keymap.set
+
+keyset("n", "gd", "<Plug>(coc-definition)", {silent = true})
+keyset("n", "gy", "<Plug>(coc-type-definition)", {silent = true})
+keyset("n", "gi", "<Plug>(coc-implementation)", {silent = true})
+keyset("n", "gr", "<Plug>(coc-references)", {silent = true})
+
+function _G.show_docs()
+    local cw = vim.fn.expand('<cword>')
+    if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
+        vim.api.nvim_command('h ' .. cw)
+    elseif vim.api.nvim_eval('coc#rpc#ready()') then
+        vim.fn.CocActionAsync('doHover')
+    else
+        vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+    end
+end
+keyset("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
+
+local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
+keyset("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
+keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
+
+keyset("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
+vim.cmd('nnoremap <silent><nowait> <leader>d  :call CocAction(\'jumpDefinition\', v:false)<CR>')
 
 --
 -- Back-end
@@ -160,6 +182,8 @@ vim.api.nvim_set_keymap("n", "<leader>6", "6gt", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>7", "7gt", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>8", "8gt", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>9", "9gt", { noremap = true })
+
+vim.api.nvim_set_keymap("n", "<leader>q", ":q<CR>", { noremap = true })
 
 --
 -- Language specific settings
