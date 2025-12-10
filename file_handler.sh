65ac200f45ca41813e484e9743dcde3db80ec239
Steve Nekoliczak <steve.nekoliczak@protonmail.com>
2025-12-10 12:59:39 -0600

    Update `file_handler.sh` to remove lines for files that don't exist anymore

--- file_handler.sh
+++ file_handler.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-ln -s ~/.unix_setup/config_files/.xinitrc ~/.xinitrc
-ln -s ~/.unix_setup/config_files/.Xresources ~/.Xresources
 ln -s ~/.unix_setup/config_files/.zshrc ~/.zshrc
 ln -s ~/.unix_setup/config_files/.gitignore ~/.gitignore
 ln -s ~/.unix_setup/config_files/.tmux.conf ~/.tmux.conf
 mkdir -p ~/.config/nvim/
 ln -s ~/.unix_setup/config_files/init.lua ~/.config/nvim/init.lua
-ln -s ~/.unix_setup/config_files/coc-settings.json ~/.config/nvim/coc-settings.json
 mkdir -p ~/.config/kitty/
 ln -s ~/.unix_setup/config_files/kitty.conf ~/.config/kitty/kitty.conf
 ln -s ~/.unix_setup/config_files/.ideavimrc ~/.ideavimrc
